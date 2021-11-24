cf1ff77565cfc7a83b28c99b2ac27a1528215246
Xunorus <xunorus@gmail.com>
2021-11-24 16:37:15 +0100

    quick fix css navbar

--- js/minter.js
+++ js/minter.js
@@ -228,7 +228,8 @@
      // const TOKEN_CONTRACT_ADDRESS = "0x0dB4DF5E0FF20aCF2E3469d5756103A3e03cd1a1"; // creado con nftport
      const NFTPORT_KEY = '524a1fad-f13f-4317-8ad0-0e75e45d4b61';
      const TOKEN_CONTRACT_ADDRESS = "0x95c5B3a20e6f8a352B82236271756Ed83d4030b0"; // creado con nftport
-     const TO_ADDRESS = "0x9031FDE86c3c9115d302043316E97dD9ef5d3e66"; // creado con nftport
+    //  const TO_ADDRESS = "0x9031FDE86c3c9115d302043316E97dD9ef5d3e66"; //nft owner. dir xun 
+     const TO_ADDRESS = "0xe4458d575e5D6867e6A9e52dED44cef8c0F888F8"; // nft owner.dir nat
      const CHAIN = "polygon";
 
      const settings = {
